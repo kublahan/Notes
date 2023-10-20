956aea46c56117bce0610f74147197fb3a63848b
kublahan <67871033+kublahan@users.noreply.github.com>
2023-10-20 15:17:32 +0300

    code optimize

--- Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQuery.cs
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQuery.cs
@@ -6,6 +6,5 @@ namespace Notes.Application.Notes.Queries.GetNoteDetails
     public class GetNoteDetailsQuery : IRequest<NoteDetailsVm>
     {
         public Guid UserId { get; set; }
-        public Guid Id { get; set; }
     }
 }
